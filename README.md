bd8ec276da65e6560a3abeed3309a6c2c0adcd0d
lejibxl <57529300+lejibxl@users.noreply.github.com>
2020-01-28 15:14:15 +0100

    a

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pycom-bq25895
+![Total visitor](https://visitor-count-badge.herokuapp.com/total.svg?repo_id=lejibxl.pycom-bq25895)
+![Visitors in today](https://visitor-count-badge.herokuapp.com/today.svg?repo_id=lejibxl.pycom-bq25895)
--- bq25895.py
+++ bq25895.py
@@ -1,4 +1,6 @@
-from machine import I2C
+from machine import I2C,ADC,Pin,Timer
+import utime
+import pycom
 import ubinascii
 VBUS_TYPE =['NONE',
             'SDP',
@@ -20,9 +22,72 @@ PG_STAT =  ['Not Power Good',
 SDP_STAT = ['USB100 input is detected',
             'USB500 input is detected']
 
-#i2c = I2C(0)
-#i2c.writeto_mem(0x6A,0x00,0b00111111)
-#bin(i2c.readfrom_mem(0x6A, 0x0B, 1)[0])
+class POWER:
+    # voltage taken from ILIM and amplified by an ampli op
+    def __init__(self):
+        self.adc = ADC()
+        self.p_SHDN_ = Pin('P21', mode=Pin.OUT) #shutdown/enable ampli op
+        self.pwr_ticks = utime.ticks_us()
+        try:
+            pycom.nvs_get("pwr_uAH")
+        except Exception as e:
+            pycom.nvs_set("pwr_uAH",0)
+        self.pwr_nAH = 0
+        self.seconds = 0
+        self.__alarm = Timer.Alarm(self.mesure, 1, periodic=True)
+
+    def _seconds_handler(self, alarm):
+        self.seconds += 1
+        print("%02d seconds have passed" % self.seconds)
+        if self.seconds == 10:
+            alarm.cancel() # stop counting after 10 seconds
+
+    def reset(self):
+        self.pwr_nAH = 0
+        pycom.nvs_set("pwr_uAH",0)
+
+    def getPWR(self):
+        RILIM = 130
+        KILIM = 365
+        GAIN = 12.12
+        self.p_SHDN_.value(1)
+        utime.sleep_us(10) #Enable Delay Time from Shutdown
+        #ADC.ATTN_0DB (0-1), ADC.ATTN_2_5DB(0-1.33), ADC.ATTN_6DB(0-2), ADC.ATTN_11DB(0-3.55)
+        ADC_GAIN=[0,1.334,1.995,3.548]
+        for i, e in reversed(list(enumerate(ADC_GAIN))):
+            adc_ILIM = self.adc.channel(attn=i,pin='P20')
+            ILIM = adc_ILIM()
+            if ILIM < 2000 and i > 0:
+                pass
+            else:
+                VILIM = adc_ILIM.voltage()
+                break
+        PWIN_I = (KILIM * (VILIM / GAIN)) / (RILIM * 0.8)
+        print("ADC : {}, ADC_v = {}, PWIN_I : {}".format(adc_ILIM(), VILIM, PWIN_I) )
+        self.p_SHDN_.value(0)
+        return PWIN_I
+
+
+    def mesure(self, alarm):
+        old = self.pwr_ticks
+        self.pwr_ticks = utime.ticks_us()
+        delta = utime.ticks_diff(self.pwr_ticks , old)
+        self.pwr_nAH = self.pwr_nAH + int(self.getPWR()  * delta / 3600)
+        uAH = self.pwr_nAH // 1000
+        print("nAH : {}, uAH : {}".format(self.pwr_nAH, uAH))
+        if uAH > 0 :
+            self.pwr_nAH = self.pwr_nAH  - (uAH * 1000)
+            uAH = pycom.nvs_get("pwr_uAH") + uAH
+            print("pwr_uAH :{}".format(uAH))
+            pycom.nvs_set("pwr_uAH", uAH )
+
+    @property
+    def pwr_uAH(self):
+        return pycom.nvs_get("pwr_uAH")
+
+    @property
+    def pwr_mAH(self):
+        return int(round(pycom.nvs_get("pwr_uAH")/1000) )
 
 class BQ25895:
     I2CADDR=const(0x6A)
@@ -168,36 +233,41 @@ if __name__ == '__main__':
     import time
     from machine import ADC
     bq25895=BQ25895()
+    power=POWER()
     bq25895.reset()
     time.sleep_ms(550)
     bq25895._setBit(0x14,[1,None,None,None,None,None,None,None])
+    a="1"
+    if a == "1":
+        #__alarm = Timer.Alarm(power.mesure, ms=1000, periodic=True)
+        while True:
+            #power.mesure(1)
+            print("pwr_nAH : {}".format(power.pwr_nAH))
+            time.sleep(2)
+    elif a == "2":
+        while True:
+            #bq25895.set_input_current_max(3250)
+            #bq25895._setBit(0x07,[None,None,0,0,None,None,None,None])
+            print("status: {}, TS: {}".format(bq25895.read_stat(),bq25895.read_TS_per()))
+            print("IN Stat: {}, vbus:{}.{}, in Imax:{}, in U:{}".format(bq25895.pg_stat_str(),bq25895.vbus_type(),bq25895.vbus_type_str(), bq25895.read_input_current_max(),bq25895.read_vbus_volt()))
+            print("Bat stat:{}, I:{}, V:{}".format(bq25895.chrg_stat_str(), bq25895.read_charge_current(), bq25895.read_battery_volt()))
+            print("SYS status:{}, V:{}".format(bq25895.vsys_stat_str(), bq25895.read_sys_volt()))
+            print("PMID Boost status" )
 
-    adc = ADC(bits=12)
-    adc_ILIM = adc.channel(attn=ADC.ATTN_0DB,pin='P20')
+            VILIM = adc_ILIM.voltage()
+            RILIM = 768
+            KILIM = 365
+            PWIN_I = (KILIM * VILIM) / (RILIM * 0.8)
+            print("RPY_I:{}, CRG_I:{} ".format(PWIN_I,bq25895.read_charge_current()))
+            print("pwr_uAH:{}, power:{}".format(power.pwr_uAH, power.getPWR()))
 
-    while True:
-        #bq25895.set_input_current_max(3250)
-        #bq25895._setBit(0x07,[None,None,0,0,None,None,None,None])
-        print("status: {}, TS: {}".format(bq25895.read_stat(),bq25895.read_TS_per()))
-        print("IN Stat: {}, vbus:{}.{}, in Imax:{}, in U:{}".format(bq25895.pg_stat_str(),bq25895.vbus_type(),bq25895.vbus_type_str(), bq25895.read_input_current_max(),bq25895.read_vbus_volt()))
-        print("Bat stat:{}, I:{}, V:{}".format(bq25895.chrg_stat_str(), bq25895.read_charge_current(), bq25895.read_battery_volt()))
-        print("SYS status:{}, V:{}".format(bq25895.vsys_stat_str(), bq25895.read_sys_volt()))
-        print("PMID Boost status" )
-
-        VILIM = adc_ILIM.voltage()
-        RILIM = 768
-        KILIM = 365
-        PWIN_I = (KILIM * VILIM) / (RILIM * 0.8)
-        print("RPY_I:{}, CRG_I:{} ".format(PWIN_I,bq25895.read_charge_current()))
-
-
-        """
-            print("##")
-            print("IN Stat: {}, vbus:{:03b}.{}, in Imax:{}, in U:{}".format(bq25895.pg_stat_str(),bq25895.vbus_type(),bq25895.vbus_type_str(), bq25895.read_input_current_max(),bq25895.read_vbus_volt()))
-            i=0
-            while i <= 0x14:
-                print("{:02x}:{:08b}".format(i,bq25895.readBit(i)), end=' ')
-                i = i + 1
-            print("---")
-        """
-        time.sleep(3)
+            """
+                print("##")
+                print("IN Stat: {}, vbus:{:03b}.{}, in Imax:{}, in U:{}".format(bq25895.pg_stat_str(),bq25895.vbus_type(),bq25895.vbus_type_str(), bq25895.read_input_current_max(),bq25895.read_vbus_volt()))
+                i=0
+                while i <= 0x14:
+                    print("{:02x}:{:08b}".format(i,bq25895.readBit(i)), end=' ')
+                    i = i + 1
+                print("---")
+            """
+            time.sleep(3)
